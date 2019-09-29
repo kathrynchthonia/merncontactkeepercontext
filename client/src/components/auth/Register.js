e315ac78bd9b50a0f806f649d65dc4a62428ba41
twheelertech <tony@wheelertechconsulting.com>
2019-09-28 19:01:36 -0500

    Added register user functionality

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,21 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import AlertContext from "../../context/alert/alertContext";
+import AuthContext from "../../context/auth/authContext";
 
 const Register = () => {
   const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
 
+  const { register, error, clearErrors } = authContext;
   const { setAlert } = alertContext;
+
+  useEffect(() => {
+    if (error === "User already exists") {
+      setAlert(error, "danger");
+      clearErrors();
+    }
+  }, [error]);
+
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -23,7 +34,11 @@ const Register = () => {
     } else if (password !== password2) {
       setAlert("Passwords do not Match...", "danger");
     } else {
-      console.log("Register Submit");
+      register({
+        name,
+        email,
+        password
+      });
     }
   };
 
