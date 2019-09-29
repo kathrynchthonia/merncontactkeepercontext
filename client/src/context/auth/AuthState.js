b7ba8ce04f3ef300e794d74615f57ff260b3d12b
twheelertech <tony@wheelertechconsulting.com>
2019-09-28 20:07:50 -0500

    Added register user authentication

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -12,6 +12,7 @@ import {
   LOGOUT,
   CLEAR_ERRORS
 } from "../types";
+import setAuthToken from "../../utils/setAuthToken";
 
 const AuthState = props => {
   const initialState = {
@@ -25,7 +26,22 @@ const AuthState = props => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   // Load User
-  const loadUser = () => console.log("laod user...");
+  const loadUser = async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+
+    try {
+      const res = await axios.get("/api/auth");
+
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({ type: AUTH_ERROR });
+    }
+  };
 
   // Register User
   const register = async formData => {
@@ -41,6 +57,7 @@ const AuthState = props => {
         type: REGISTER_SUCCESS,
         payload: res.data
       });
+      loadUser();
     } catch (err) {
       dispatch({
         type: REGISTER_FAIL,
